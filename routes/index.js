d7c670213894ffc430d9471c1b5228d7e15c98a3
bigfoot110614 <826160425@qq.com>
2016-08-05 21:35:28 +0800

    ip

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+markdown = require('markdown').markdown;
 
 router.get('/', function(req, res, next) {
   res.redirect('/article/list');
--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 module.exports = {
     cookieSecret: 'bigfoot',// 用于 Cookie 加密与数据库无关
-    //db: 'blog',// 数据库的名称
+    db: 'blog',// 数据库的名称
     //host: '123.57.143.189', //数据库的地址
     //port: 27017,  //数据库的端口号
-    //url: "mongodb://123.57.143.189:27017/bigfootblog",
-    url: "mongodb://localhost:27017/bigfootblog",
+    url: "mongodb://123.57.143.189:27017/bigfootblog",
+    //url: "mongodb://localhost:27017/bigfootblog",
 };
